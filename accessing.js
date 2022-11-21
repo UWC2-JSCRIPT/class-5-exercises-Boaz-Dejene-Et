df28ae377ad0f5b83550bd97c85055a78f21ccff
Boaz-Dejene-Et <boazdeju@gmail.com>
2022-11-21 16:32:56 +0300

    update

--- accessing.js
+++ accessing.js
@@ -1,5 +1,11 @@
 // Change the text of the "Seattle Weather" header to "February 10 Weather Forecast, Seattle"
-
+document.getElementById("weather-head").innerHTML = "February 10 Weather Forecast, Seattle"
 // Change the styling of every element with class "sun" to set the color to "orange"
+let sun = document.getElementsByClassName('sun')
+sun[0].style.color = "orange"
+// !--- i have added additional option in the css file line 33 ---! //
+console.log(sun[0])
 
+// document.getElementById('weather-head').style.color = "red"
 // Change the class of the second <li> from to "sun" to "cloudy"
+document.getElementsByTagName("li")[1].className = "cloudy"
--- dom-crud.js
+++ dom-crud.js
@@ -1,14 +1,23 @@
 // Create a new <a> element containing the text "Buy Now!" 
 // with an id of "cta" after the last <p>
+var a = document.createElement('a');
+var linkText = document.createTextNode("Buy Now!");
+a.appendChild(linkText);
+a.title = "Buy Now!";
+a.href = "http://example.com";
+a.id = "cta";
+document.body.appendChild(a);
 
 
 // Access (read) the data-color attribute of the <img>,
 // log to the console
 
+console.log(document.getElementsByTagName("img")[0].dataset.color)
 
 // Update the third <li> item ("Turbocharged"), 
 // set the class name to "highlight"
 
+document.getElementsByTagName("li")[2].className = "highlight"
 
 // Remove (delete) the last paragraph
 // (starts with "Available for purchase now…")
