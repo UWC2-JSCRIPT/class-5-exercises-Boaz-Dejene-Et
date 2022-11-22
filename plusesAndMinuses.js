1d918cc3bb047987afdaaf44df0bd340c99fe068
boaz dejene <boazdeju@gmail.com>
2022-11-22 21:19:57 +0300

    submit

--- plusesAndMinuses.js
+++ plusesAndMinuses.js
@@ -1,6 +1,5 @@
 // When a user clicks the + element, the count should increase by 1 on screen.
 // When a user clicks the – element, the count should decrease by 1 on screen.
-
 let count = 0;
 
 function addCount() {
