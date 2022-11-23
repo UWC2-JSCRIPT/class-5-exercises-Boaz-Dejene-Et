5f35d30fdbd9bb478957981b53c1ddb5e879408c
boaz dejene <boazdeju@gmail.com>
2022-11-23 05:01:02 +0300

    submit

--- accessing.js
+++ accessing.js
@@ -2,9 +2,11 @@
 document.getElementById("weather-head").innerHTML = "February 10 Weather Forecast, Seattle"
 // Change the styling of every element with class "sun" to set the color to "orange"
 let sun = document.getElementsByClassName('sun')
-sun[0].style.color = "orange"
-// !--- i have added additional option in the css file line 33 ---! //
-console.log(sun[0])
+// sun[0].style.color = "orange"
+for (var i = 0; i < sun.length; i++) {
+    // tags[i].addEventListener('click', done,false);
+    sun[i].style.color = "orange"
+  }
 
 // Change the class of the second <li> from to "sun" to "cloudy"
 document.getElementsByTagName("li")[1].className = "cloudy"
